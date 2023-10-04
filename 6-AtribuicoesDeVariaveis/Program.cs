1410aedeb1e610d6df988a2c7b4f41e4b03a858c
juanprado <juan.prado@cacaushow.com.br>
2023-10-04 07:12:57 -0300

    Como funciona atribuição de variaveis

---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 6 - Atribuições de variaveis");
+
+        int idade = 30;
+        int idadeAna = idade;
+
+        // Aqui via imprimir 30 pois está mostrando antes de atribuir 25 a variavel idade
+        Console.WriteLine(idadeAna);
+
+        idade = 25;
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
