f4277700b43cd685dcace6ecc0a897dc9797f9cf
juanprado <juan.prado@cacaushow.com.br>
2023-10-04 07:22:49 -0300

    Como usar a condição if no C#

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 7 - Condicionais");
+
+        int idadeJuan = 16;
+        int quantidadePessoas = 2;
+
+        if (idadeJuan >= 18)
+        {
+            Console.WriteLine("Pode Entrar!");
+        }
+        else
+        {
+            if (quantidadePessoas > 1)
+            {
+                Console.WriteLine("Ele está acompanhado, pode entrar!");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar!");
+            }
+        }
+
+        Console.WriteLine("Tecle Enter para fechar");
+        Console.ReadLine();
+    }
+}
