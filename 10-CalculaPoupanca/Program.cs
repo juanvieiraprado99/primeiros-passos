5bb4175cba27f01d3344aa3da96d5e0d456786c9
juanprado <juan.prado@cacaushow.com.br>
2023-10-05 07:33:17 -0300

    Laço de repetição while

---
+++ 10-CalculaPoupanca/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 10 - Calcula Poupança");
+
+        double investimento = 1000;
+
+        // Rendimento de 0.5% ao mês
+
+        //investimento = investimento + investimento * 0.05;
+        //Console.WriteLine(investimento);
+
+        int mes = 1;
+
+        while (mes <= 12)
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine(investimento);
+            mes ++;
+        }
+
+        Console.WriteLine("Tecle Enter para fechar...");
+        Console.ReadLine();
+    }
+}
