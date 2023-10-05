3dd1de81be7758130e1f2e41007cde6f0c86470e
juanprado <juan.prado@cacaushow.com.br>
2023-10-05 07:50:31 -0300

    Utilizando break para parar uma condição

---
+++ 13-EncadeandoFor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Executnado o projeto 13 - Encadeando For");
+
+        for (int contadorLinhas = 0; contadorLinhas < 10; contadorLinhas++)
+        {
+            for (int contadorColunas = 0; contadorColunas < 10; contadorColunas++)
+            {
+                Console.Write("*");
+                if (contadorColunas >= contadorLinhas)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Tecle Enter para fechar");
+        Console.ReadLine();
+    }
+}
