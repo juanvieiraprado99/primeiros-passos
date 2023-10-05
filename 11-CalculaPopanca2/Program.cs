9c6bdfa19adafce547cc01d73e8927439046def3
juanprado <juan.prado@cacaushow.com.br>
2023-10-05 07:36:33 -0300

    Laço de repetição for

---
+++ 11-CalculaPopanca2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 11 - Calcula Poupança 2");
+
+        double investimento = 1000;
+
+        int mes = 12;
+
+        for (int i = 0; i < mes; i++)
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine(investimento);
+        }
+
+        Console.WriteLine("Tecle Enter para fechar...");
+        Console.ReadLine();
+    }
+}
