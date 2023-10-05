116016bcebde1005859c90c2cd182303af6857c0
juanprado <juan.prado@cacaushow.com.br>
2023-10-05 07:25:09 -0300

    Condicionais e Escopo

---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 8 - Condicionais 2");
+
+        int idadeJuan = 16;
+        int quantidadePessoas = 2;
+
+        bool acompanahado = quantidadePessoas > 1;
+        bool grupo = true;
+
+        if (idadeJuan >= 18 && acompanahado)
+        {
+            Console.WriteLine("Pode Entrar!");
+        }
+        else
+        {
+            Console.WriteLine("Não pode entrar!");
+        }
+
+        if (idadeJuan >= 18 || acompanahado)
+        {
+            Console.WriteLine("Pode Entrar!");
+        }
+        else
+        {
+            Console.WriteLine("Não pode entrar!");
+        }
+
+        Console.WriteLine("Tecle Enter para fechar");
+        Console.ReadLine();
+    }
+}
---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 8 - Condicionais 2");
+
+        int idadeJuan = 16;
+        int quantidadePessoas = 2;
+
+        bool acompanahado = quantidadePessoas > 1;
+
+        string textoAdicional;
+
+        if (acompanahado == true)
+        {
+            textoAdicional = "João está acompanhado";
+        } 
+        else
+        {
+            textoAdicional = "João não está acompanhado";
+        }
+
+        if (idadeJuan >= 18 && acompanahado)
+        {
+            Console.WriteLine("Pode Entrar!");
+            Console.WriteLine(textoAdicional);
+
+        }
+        else
+        {
+            Console.WriteLine("Não pode entrar!");
+            Console.WriteLine(textoAdicional);
+        }
+
+        Console.WriteLine("Tecle Enter para fechar");
+        Console.ReadLine();
+    }
+}
